075ba3c7e9e803eb553eab28875d959d86d62f19
andrea1632 <andrea.bellucci10@libero.it>
2022-05-25 16:47:16 +0200

    added cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,13 +12,30 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
-    <body>
-        <div>
-            @foreach ($movies as $key => $movie)
-                <div class="p-4 text-primary">
-                    {{$movie->title}}
-                </div>
-            @endforeach
+    <header>
+        <h1 class="text-center text-success py-4">
+            Films
+        </h1>
+    </header>
+    <body class="bg-dark">
+        <div class="container">
+            <div class="row p-4 justify-content-center">
+                @foreach ($movies as $key => $movie)
+                    <div class="card col-3 m-2">
+                        <img class="w-50 align-self-center" src="https://previews.123rf.com/images/alexeysmirnov/alexeysmirnov1910/alexeysmirnov191000048/136434981-aprire-il-ciak-nero-film-isolato-su-sfondo-bianco-cinema-cinema-attrezzature-per-l-industria-cinemat.jpg?fj=1" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">Titolo: {{$movie->title}}</h5>
+                        <h6 class="card-title mb-5">Titolo originale: {{$movie->original_title}}</h6>
+                        <div class="card-text">
+                            <p>Nazionalità: {{$movie->nationality}}</p>
+                            <p>Data Uscita: {{$movie->date}}</p>
+                            <p>Voto: {{$movie->vote}}</p>
+                        </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+ 
         </div>
     </body>
 </html>
